43dccdbb1fd89af28cf28a27a8b9d238a2b792e0
Carla Fernandez <carlafgpitando@gmail.com>
2019-03-10 18:54:46 +0100

    Fitness and move

--- sources/particle.py
+++ sources/particle.py
@@ -1,6 +1,5 @@
 import random
 from collections import defaultdict
-
 import numpy as np
 import sklearn.metrics
 
@@ -8,8 +7,8 @@ import sklearn.metrics
 class Particle:
     MIN_POS = 0
     MAX_POS = 100
-    MIN_VEL = 5
-    MAX_VEL = 50
+    MIN_VEL = 1
+    MAX_VEL = 3
 
     def __init__(self, doc_vectors, num_clusters):
         self.centroid_vecs = []
@@ -23,17 +22,15 @@ class Particle:
 
             self.centroid_vecs.append(chosen_vec)
 
-        # self.position = random.randint(self.MIN_POS, self.MAX_POS)
-        # self.velocity = random.randint(self.MIN_VEL, self.MAX_VEL)
-        # self.best_pos = self.position
-        # self.best_val = np.inf
+        self.position = random.randint(self.MIN_POS, self.MAX_POS)
+        self.velocity = random.randint(self.MIN_VEL, self.MAX_VEL)
 
-    def assign_closest_centroid(self, distance_metric):
-
+    def assign_closest_centroids(self, distance_metric):
         # for each document vector, check the closest centroid and assign it
         for i in range(self.doc_vecs.shape[0]):
             smallest_distance = np.inf
             current_closest = []
+
             for j in range(len(self.centroid_vecs)):
                 centroid = self.centroid_vecs[j]
                 distance = self.measure_distance(centroid, self.doc_vecs[i], distance_metric)
@@ -46,17 +43,42 @@ class Particle:
 
     def measure_distance(self, centroid, vector, metric):
         if metric == 'euclidean':
-            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="euclidean")
+            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="euclidean")[0][0]
         elif metric == 'cosine':
-            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="cosine")
+            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="cosine")[0][0]
         else:
-            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="euclidean")
+            return sklearn.metrics.pairwise.pairwise_distances(centroid, vector, metric="euclidean")[0][0]
 
-    def move(self):
-        # the particle changes position according to its velocity and inertia
-
+    def move(self, inertia, cognitive, social, best_position):
         # the particle updates current velocity values
-        pass
+        self.velocity = inertia * self.velocity + \
+            cognitive * random.randint(0, 1) * abs(best_position - self.position)
+            #  + social * random.randint() * (best_position-self.position)
+        if (self.velocity > self.MAX_VEL):
+            self.velocity = self.velocity % self.MAX_VEL
 
-    def calculate_fitness(self):
-        pass
+        # the particle changes position according to its velocity and wraps around
+        self.position += self.velocity
+        if (self.position > self.MAX_POS):
+            self.position = self.position % self.MAX_POS
+
+        return self.position
+
+
+    def calculate_fitness(self, metric):
+        addc = 0
+        for key in range(len(self.assigned.keys())):
+            centroid_cum  = 0
+
+            # for each vector assigned to the centroid
+            for value in range(len(self.assigned[key])):
+                vector = self.assigned[key][0]
+                centroid_cum += self.measure_distance(vector, self.centroid_vecs[key], metric)
+
+            # if there were any vectors, do the average, else 0
+            if len(self.assigned[key]) != 0:
+                addc += centroid_cum / len(self.assigned[key])
+            else:
+                addc += 0
+
+        return addc / len(self.assigned.keys())
--- sources/pso.py
+++ sources/pso.py
@@ -1,5 +1,5 @@
 from particle import Particle
-
+import numpy as np
 
 class PSO:
     def __init__(self, data):
@@ -21,25 +21,36 @@ class PSO:
 
         # TODO remove this, used as placeholder for stop condition
         num_iter = 0
-
+        best_particle = self.particles[0]
 
         # for every iteration until the stop condition is met
         while not self.stop_condition_met(num_iter):
+            best_fitness = np.inf
             for i in range(num_particles):
                 particle = self.particles[i]
 
                 # assign each document vector to the closest centroid
                 print("Finding closest centroid vectors -- Particle {0}".format(i))
-                particle.assign_closest_centroid(distance_metric)
+                particle.assign_closest_centroids(distance_metric)
 
                 # each particle calculates and stores its new fitness value
-                particle.calculate_fitness()
+                fitness = particle.calculate_fitness(distance_metric)
+                if fitness < best_fitness:
+                    best_fitness = fitness
+                    best_position = particle.position
+                    best_particle = particle
+                print("Fitness value for particle {0} -- {1}".format(i, fitness))
 
                 # each particle moves according the its velocity and inertia (acceleration???)
                 # and updates those values accordingly
-                particle.move()
+                new_position = particle.move(inertia, cognitive, social, best_position)
+                print("\nParticle {0} is now at position {1}\n".format(i, new_position))
 
             num_iter += 1
+
+        print("Execution complete. Final document cluster assignment:")
+        for k, v in best_particle.assigned.items():
+            print(k, v)
 
     def initialize_particles(self, num_clusters, num_particles):
         # each particle randomly chooses k different document vectors
@@ -47,7 +58,10 @@ class PSO:
 
         for i in range(num_particles):
             print("Choosing initial clusters -- Particle {0}".format(i))
-            self.particles.append(self.createParticle(self.data, num_clusters))
+            particle = self.createParticle(self.data, num_clusters)
+            print("New particle created with position {0} and velocity {1}"
+                  .format(particle.position, particle.velocity))
+            self.particles.append(particle)
 
     def createParticle(self, doc_vectors, num_clusters):
         return Particle(doc_vectors, num_clusters)
@@ -56,6 +70,6 @@ class PSO:
         # TODO more stopping conditions
         # stop condition may be either a maximum number of iterations
         # or the difference between updates in centroid vectors (improvement in fitness value???)
-        return num_iter == 1
+        return num_iter == 2
 
 
